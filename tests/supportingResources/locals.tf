2463c6cce1f8a9071f7646367d76a3b4ab930349
john <jmwangi288@gmail.com>
2023-02-01 00:08:15 +0300

    added new github workflow files

---
+++ tests/supportingResources/locals.tf
@@ -0,0 +1,16 @@
+locals {
+  cidr_block          = "10.1.0.0/16"
+  database_subnets    = [cidrsubnet(local.cidr_block, 8, 13), cidrsubnet(local.cidr_block, 8, 14), cidrsubnet(local.cidr_block, 8, 15)]
+  name            = "terraform-aws-rds-aurora" 
+
+  tags = {
+    Environment        = "examples"
+    "user::CostCenter" = "terraform-registry"
+    department         = "operations"
+    InstanceScheduler  = true
+    Project            = "aws-vpc"
+    Owner              = "hugo.almeida"
+    LayerName          = "c300-aws-vpc"
+    LayerId            = "c300"
+  }
+}
---
+++ tests/supportingResources/main.tf
@@ -0,0 +1,23 @@
+module "aurora_vpc" {
+  source               = "boldlink/vpc/aws"
+  version              = "3.0.2"
+  name                 = local.name
+  cidr_block           = local.cidr_block
+  enable_internal_subnets = true
+
+  internal_subnets = {
+    databases = {
+      cidrs        = local.database_subnets
+    }
+  }
+}
+
+module "kms_key" {
+  source                  = "boldlink/kms/aws"
+  description             = "A test kms key for rds"
+  alias_name              = "alias/${local.name}-alias"
+  create_kms_alias        = true
+  enable_key_rotation     = true
+  deletion_window_in_days = 7
+}
+

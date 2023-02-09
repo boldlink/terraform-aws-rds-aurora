4d70004c8a6d37827db0077e473470fae6cd5f02
Boldlink-SIG automation user <106749487+boldlinksig@users.noreply.github.com>
2023-02-09 20:35:13 +0300

    [automation] updated files automatically. (#7)
    * Updated files
    
    * added new checkov files to be skipped in main example
    
    ---------
    
    Co-authored-by: boldlinksig <boldlinksig@boldlink.io>
    Co-authored-by: john <jmwangi288@gmail.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -37,6 +37,7 @@ module "primary_cluster" {
   source = "../../"
   #checkov:skip=CKV_AWS_96:Ensure all data stored in Aurora is securely encrypted at rest
   #checkov:skip=CKV2_AWS_27: "Ensure Postgres RDS as aws_rds_cluster has Query Logging enabled"
+  #checkov:skip=CKV_AWS_139:Ensure that RDS clusters have deletion protection enabled
   instance_count                  = 1
   availability_zones              = data.aws_availability_zones.available.names
   global_cluster_identifier       = local.global_cluster_identifier
@@ -117,6 +118,7 @@ module "secondary_cluster" {
   #checkov:skip=CKV_AWS_162:Ensure RDS cluster has IAM authentication enabled
   #checkov:skip=CKV_AWS_118:Ensure that enhanced monitoring is enabled for Amazon RDS instances
   #checkov:skip=CKV2_AWS_8:Ensure that RDS clusters has backup plan of AWS Backup
+  #checkov:skip=CKV2_AWS_27: "Ensure Postgres RDS as aws_rds_cluster has Query Logging enabled"
   primary_cluster                     = false
   instance_count                      = 1
   availability_zones                  = data.aws_availability_zones.secondary.names
